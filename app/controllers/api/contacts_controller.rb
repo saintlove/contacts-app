286ab6b4fd86a7211770789746c7efc5bce2a343
saintlove <saintlovett@gmail.com>
2018-05-17 13:36:15 -0500

    migrations

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,7 +1,7 @@
 class Api::ContactsController < ApplicationController
   def index
-   @contacts = Contact.all 
-   render 'index.json.jbuilder'
+    @contacts = Contact.all
+    render 'index.json.jbuilder'
   end
 
   def show
@@ -12,11 +12,14 @@ class Api::ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(
-                          first_name: params[:first_name],
-                          last_name: params[:last_name],
-                          email: params[:email],
-                          phone_number: params[:phone_number]
-)
+                           first_name: params[:first_name],
+                           middle_name: params[:middle_name],
+                           last_name: params[:last_name],
+                           email: params[:email],
+                           phone_number: params[:phone_number],
+                           bio: params[:bio]
+                          )
+
     @contact.save
     render 'show.json.jbuilder'
   end
@@ -26,10 +29,12 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find(contact_id)
 
     @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
-
+    @contact.bio = params[:bio] || @contact.bio
+    
     @contact.save
     render 'show.json.jbuilder'
   end
@@ -37,8 +42,7 @@ class Api::ContactsController < ApplicationController
   def destroy
     contact_id = params[:id]
     @contact = Contact.find(contact_id)
-    contact.destroy
-    render json: {message: "Contact successfully deleted."}
+    @contact.destroy
+    render json: {message: "contact successfully destroyed"}
   end
 end
-
